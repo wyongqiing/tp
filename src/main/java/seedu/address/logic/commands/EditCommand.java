40cc07cc1ecd143fee060109b3cf8f3f0788d098
davidfoo007 <tech@myunilah.com>
2025-03-18 23:56:47 +0800

    Removed trailing whitespaces

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -86,7 +86,6 @@ public class EditCommand extends Command {
 
         if (index.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-            
         }
 
         Person personToEdit = lastShownList.get(index.getZeroBased());
